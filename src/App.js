9abc069e30cedac8fa86acf6b49e2967c75519a9
SanketDhabarde <sanketdhabarde13@gmail.com>
2021-05-29 12:13:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
 import './App.css';
+import Chat from './components/Chat/Chat';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__serverLogo">
+        <Avatar/>
+        <Avatar/>
+        <Avatar/>
+        <IconButton>
+            <AddIcon/>
+        </IconButton>
+      </div>
+      <Sidebar/>
+      <Chat/>
     </div>
   );
 }
